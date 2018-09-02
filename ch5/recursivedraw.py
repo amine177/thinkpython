089c7d9a5fd73be0e0a28332218a8ddc2633b69a
aminos <amri.amine@protonmail.com>
2018-09-02 20:51:33 +0100

    recursive draw

---
+++ ch5/recursivedraw.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+def draw(t, length, n):
+    if n == 0:
+        return
+    angle = 50
+    t.fd(length*n)
+    t.lt(angle)
+    draw(t, length, n-1)
+    t.rt(2*angle)
+    draw(t, length, n-1)
+    t.lt(angle)
+    t.bk(length*n)
+
+
+bob = turtle.Turtle()
+bob.lt(-90)
+bob.fd(300)
+bob.lt(90)
+draw(bob, 10, 30)
+turtle.mainloop()
