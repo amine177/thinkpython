5ee1fb801e917329deecb3f761827d97bc526ac1
aminos <amri.amine@protonmail.com>
2018-09-07 16:29:55 +0100

    plot frequency of word versus rank

---
+++ ch13/zipf.py
@@ -0,0 +1,22 @@
+import filetowords as analyze
+from math import log
+import matplotlib.pyplot as plt
+
+
+def rank(file):
+    hist = analyze.histogram(analyze.wordlist(file))
+    shist = [(key, value) for key, value in sorted(hist.items(),
+                                                   key=lambda x: x[1],
+                                                   reverse=True)]
+
+    graph = []
+    for i in range(len(shist)):
+        graph.append((log(i+1), log(shist[i][1])))
+
+    plt.scatter(*zip(*graph))
+    plt.show()
+
+
+if __name__ == "__main__":
+    f = open("emma.txt")
+    rank(f)
