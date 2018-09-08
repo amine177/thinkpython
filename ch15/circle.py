879d6e74afc7f0e7e647cf7235ce54c65d59dbcb
aminos <amri.amine@protonmail.com>
2018-09-08 15:16:28 +0100

    refactoring

--- ch15/circle.py
+++ ch15/circle.py
@@ -1,14 +1,5 @@
 from math import sqrt
-
-
-class Point:
-
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    def distance(self, p):
-        return sqrt((self.x - p.x)**2 + (self.y - p.y)**2)
+from point import Point
 
 
 class Circle:
