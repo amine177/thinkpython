449d6bec8f938fcb7908912b8a0d4791281c8fd4
aminos <amri.amine@protonmail.com>
2018-09-08 12:55:57 +0100

    make file an anagrams dictionnary

--- anagram_sets.py
+++ anagram_sets.py
@@ -10,3 +10,14 @@ def isanagram(s1, s2):
 
 def nsort(s):
     return ''.join(sorted(list(s)))
+
+
+def filetoanagrams(f):
+
+    f = open(f)
+    line = f.readline()
+    d = {}
+    for w in line.split():
+        d.setdefault(nsort(w), [w]).append(w)
+
+    return d
