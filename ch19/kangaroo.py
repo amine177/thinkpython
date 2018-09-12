40cf27ca0d199f2debd72b77cc7c3f29b180999f
aminos <amri.amine@protonmail.com>
2018-09-12 15:08:20 +0100

    using conditional expressions

---
+++ ch19/kangaroo.py
@@ -0,0 +1,21 @@
+
+
+class kangaroo:
+
+    def __init__(self, p=None):
+        self.p_c = [] if p is None else p
+
+    def put_in_pouch(self, o):
+        self.p_c.append(o)
+
+    def __str__(self):
+        return 'kangaroo:' + str(self.p_c)
+
+
+if __name__ == "__main__":
+    k1 = kangaroo()
+    k1.put_in_pouch(1)
+    k2 = kangaroo()
+    k2.put_in_pouch(2)
+    print(k1)
+    print(k2)
